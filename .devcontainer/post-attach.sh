d49b5998ce981fb7d74ed3f7564272229099a9fe
vojkovic <brockv@tuta.io>
2024-02-08 13:36:39 +0800

    Fix searxng remove devcontainer

--- .devcontainer/post-attach.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "post-start start" >> ~/status
-
-if ! grep -q tty /home/vscode/.bashrc; then
-  echo "export GPG_TTY=\$(tty)" >> /home/vscode/.bashrc
-fi
-
-echo "post-start complete" >> ~/status
